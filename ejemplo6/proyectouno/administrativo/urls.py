132bdc20f4e442cba24a66022e80afa373f2b91f
msmartinez12 <msmartinez12@utpl.edu.ec>
2022-06-20 14:47:54 -0500

    cambios

--- ejemplo6/proyectouno/administrativo/urls.py
+++ ejemplo6/proyectouno/administrativo/urls.py
@@ -1,4 +1,12 @@
-
+"""
+    Manejo de urls para la aplicación
+    administrativo
+"""
 from django.urls import path
 # se importa las vistas de la aplicación
 from . import views
+
+
+urlpatterns = [
+        path('bienvenidos', views.index, name='index'),
+ ]
--- ejemplo6/proyectouno/administrativo/views.py
+++ ejemplo6/proyectouno/administrativo/views.py
@@ -6,5 +6,20 @@ from django.shortcuts import render
 # importar las clases de models.py
 from administrativo.models import Matricula
 
-# vista que permita presesentar las matriculas
-# el nombre de la vista es index.
+
+def index(request):
+    """
+        Listar los registros del modelo Estudiante,
+        obtenidos de la base de datos.
+    """
+    # a través del ORM de django se obtiene
+    # los registros de la entidad; el listado obtenido
+    # se lo almacena en una variable llamada
+    # estudiantes
+    matriculas = Matricula.objects.all()
+    # en la variable tipo diccionario llamada informacion_template
+    # se agregará la información que estará disponible
+    # en el template
+    informacion_template = {'matriculas': matriculas, 
+    'numero_matriculas': len(matriculas)}
+    return render(request, 'index.html', informacion_template)
